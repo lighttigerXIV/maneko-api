280da2481b9c5c5c42cfca78c401bed7493cf05c
lighttigerxiv <lighttigerxiv@gmail.com>
2026-05-19 16:50:12 +0100

    add: base endpoint

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from dotenv import load_dotenv
 from flask import Flask
 
+from responses import ok_response
 from routes.session import session_blueprint
 from routes.user import user_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(session_blueprint)
 app.register_blueprint(user_blueprint)
+
+
+@app.route("/", methods=["GET"])
+def root():
+    return ok_response({"message": "API is running :P"})
 
 
 app.debug = True
